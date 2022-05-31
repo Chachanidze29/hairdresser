34188f84d0aff1c4cbe5efec6797e5617cac3650
Chachanidze29 <chachanidze29m@gmail.com>
2022-05-31 14:52:59 +0400

    using HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import StoreProvider from "./utils/storeProvider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <StoreProvider>
-        <BrowserRouter>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </StoreProvider>
 );
